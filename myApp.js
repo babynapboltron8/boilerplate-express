8b673f7fdaf6e9939ee691e99786e8d746470b3c
Baby Nap Boltron <147931096+babynapboltron8@users.noreply.github.com>
2025-05-25 21:50:37 +0800

    Get Query Parameter Input from the Client

--- myApp.js
+++ myApp.js
@@ -33,4 +33,10 @@ app.get('/:word/echo', (req, res) => {
   res.json({ echo: req.params.word });
 });
 
+app.get('/name', (req, res) => {
+  const firstName = req.query.first;
+  const lastName = req.query.last;
+  res.json({ name: `${firstName} ${lastName}` });
+});
+
 module.exports = app;
